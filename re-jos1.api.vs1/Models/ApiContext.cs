5babba55c0d94b769d5532c72f3ccd56177cfeef
nellip <co.pfeiffer@gmail.com>
2020-08-27 12:37:05 +0200

    Remove ignored files

---
+++ re-jos1.api.vs1/Models/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace re_jos1.api.vs1.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base (options)
+        { }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
--- re-jos1.api.vs1/Startup.cs
+++ re-jos1.api.vs1/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using re_jos1.api.vs1.Models;
 
 namespace re_jos1.api.vs1
 {
@@ -26,6 +28,7 @@ namespace re_jos1.api.vs1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApiContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
