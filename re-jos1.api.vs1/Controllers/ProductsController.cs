8355ce4a7d7d0df9b60fcb674d5b0e5fcdcd1453
nellip <co.pfeiffer@gmail.com>
2020-08-29 14:33:42 +0200

    add product pagination

--- re-jos1.api.vs1/Controllers/ProductsController.cs
+++ re-jos1.api.vs1/Controllers/ProductsController.cs
@@ -20,22 +20,56 @@ namespace re_jos1.api.vs1.Controllers
 
         // GET /api/products
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        public async Task<ActionResult<IEnumerable<Product>>> Get(int page = 1)
         {
-            return await context.Products.OrderBy(x => x.Id).ToListAsync();
+            int pageSize = 1;
+            var products = context.Products
+                .OrderBy(x => x.Id)
+                .Include(x => x.Category)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize);
+
+            return await products.ToListAsync();
         }
 
         // GET /api/products/category
-        [HttpGet("{categoryName}")]
-        public async Task<ActionResult<IEnumerable<Product>>> GetByCategory(string categoryName)
+        [HttpGet("{slug}")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetByCategory(string slug, int page = 1)
         {
-            Category category = await context.Categories.Where(x => x.Name == categoryName).FirstOrDefaultAsync();
+            Category category = await context.Categories.Where(x => x.Slug == slug).FirstOrDefaultAsync();
             if (category == null)
             {
                 //404
                 return NotFound();
             }
-            return await context.Products.OrderBy(x => x.Id).Where(x => x.CategoryId == category.Id).ToListAsync();
+
+            int pageSize = 1;
+            var products = context.Products
+                .OrderBy(x => x.Id)
+                .Where(x => x.Category.Id == category.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize);
+
+            return await products.ToListAsync();
+        }
+
+        // GET /api/products/count/category
+        [HttpGet("count/{slug}")]
+        public async Task<ActionResult<int>> GetCount(string slug)
+        {
+           if (slug == "all")
+            {
+                return await context.Products.CountAsync();
+            }
+
+            Category category = await context.Categories.Where(x => x.Slug == slug).FirstOrDefaultAsync();
+            if (category == null)
+            {
+                //404
+                return NotFound();
+            }
+
+            return await context.Products.Where(x => x.CategoryId == category.Id).CountAsync();
         }
 
     }
